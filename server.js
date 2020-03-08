57bba25b155e215dbe710f05072678560f638c1b
Muzaffar <muzafako2@gmail.com>
2020-03-08 16:06:28 +0900

    modifies decode route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import session from "express-session";
+import { notFound, errorHandler } from "./src/middlewares/errorHandlers";
 import { decodeToken, encodeToken } from "./src/decodeEncode";
 
 const PORT = 3000;
@@ -26,15 +27,32 @@ app.post("/api/encode", async (req, res, next) => {
   try {
     const encode = await encodeToken(data, process.env.TOKEN_SECRET);
     sess[encode] = true;
-    res.status(200).json(encode);
+    res.status(200).json({ data: encode });
   } catch (err) {
-    console.log(err);
+    console.log(`ERROR ${err}`);
+    next();
   }
 });
 
-app.get("/api/decode", async (req, res, next) => {});
+app.get("/api/decode", async (req, res, next) => {
+  const { search } = req.query;
+  var sess = req.session;
+  try {
+    var newS = null;
+    if (search in sess) newS = search;
+    else throw new Error(`NOT FOUND`);
+    const decode = await decodeToken(newS);
+    res.json({ data: decode });
+  } catch (err) {
+    console.log(`ERROR ${err}`);
+    next();
+  }
+});
 
-app.delete("/api/destroy", (req, resd, next) => {});
+app.delete("/api/destroy", (req, res, next) => {});
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`sarver is running at localhost:${PORT}`);
